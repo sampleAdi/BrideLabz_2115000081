f82048771d03c91af19d676394d15d0fef719686
AdityaTiwari <tiwari.adi2008@gmail.com>
2025-01-22 19:07:31 +0530

    22 jan file2 programs

---
+++ IntOperation.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class IntOperation {
+    public static void main(String[] args) {
+        // Create a Scanner object for user input
+        Scanner sc = new Scanner(System.in);
+
+        // Input values for a, b, and c
+        System.out.print("Enter the value of a: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter the value of b: ");
+        int b = sc.nextInt();
+
+        System.out.print("Enter the value of c: ");
+        int c = sc.nextInt();
+
+        // Perform integer operations considering operator precedence
+        int result1 = a + b * c;
+        int result2 = a * b + c;
+        int result3 = c + a / b;
+        int result4 = a % b + c;
+
+        // Print the results
+        System.out.println("The results of Int Operations are:");
+        System.out.println("a + b * c = " + result1);
+        System.out.println("a * b + c = " + result2);
+        System.out.println("c + a / b = " + result3);
+        System.out.println("a % b + c = " + result4);
+    }
+}
